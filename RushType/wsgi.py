d3afa0c83b5663afd293ea31720c21febacbca20
mohamed-mehdi-hyad <mehdihyad.mh@gmail.com>
2023-01-14 18:06:00 +0100

    test hey there

--- RushType/wsgi.py
+++ RushType/wsgi.py
@@ -10,7 +10,11 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RushType.settings')
 
 application = get_wsgi_application()
+# 
+application = WhiteNoise(application, root='path/to/static/files')
+application.add_files('path/to/static/files', prefix='more-static/')
